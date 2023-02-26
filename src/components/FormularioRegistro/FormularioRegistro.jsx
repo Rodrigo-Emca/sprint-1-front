77c0dce93fe07a83cff346b3cc459a3c644daf36
Rodrigo <contactorodrigocabrera@gmail.com>
2023-02-26 20:21:28 -0300

    Register envia la petición de POST/users para crear nuevo usuario

--- src/components/FormularioRegistro/FormularioRegistro.jsx
+++ src/components/FormularioRegistro/FormularioRegistro.jsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import {useRef} from 'react'
 import './formularioRegistro.css'
+import axios from 'axios'
 
 import imgGoogle from '../../images/Google.png'
 import profileIcon from '../../images/profile.png'
@@ -7,29 +8,52 @@ import emailIcon from '../../images/@.png'
 import passwordIcon from '../../images/lock1.png'
 
 export default function FormularioRegistro() {
+
+    let name = useRef()
+    let email = useRef()
+    let password = useRef()
+
+async function handleSubmit(event) {
+        event.preventDefault()
+
+        let data = {
+            [name.current.name]: name.current.value,
+            [email.current.name]: email.current.value,
+            [password.current.name]: password.current.value
+        }
+        console.log(data)
+        let url = 'http://localhost:8080/users/'
+        try {
+            await axios.post(url, data)
+        } catch(error) {
+            console.log(error)
+            console.log('Ocurrió un error')
+        }
+    }
+
     return (
-    <form action="#" className='FormularioRegistro'>
+    <form className='FormularioRegistro' onSubmit={handleSubmit}>
         <fieldset className='innerFormulario'>
             <legend>Name</legend>
-                <input type="text" className='inputs'/>
+                <input ref={name} type="text" className='inputs' name='name' required/>
                 <img src={profileIcon} alt="profileIcon" className='icon'/>
         </fieldset>
 
         <fieldset className='innerFormulario'>
             <legend>Email</legend>
-                <input type="email" className='inputs'/>
+                <input ref={email} type="email" className='inputs' name='email' required/>
                 <img src={emailIcon} alt="emailIcon" className='icon'/>
         </fieldset>
             
         <fieldset className='innerFormulario'>
             <legend>Password</legend>
-                <input type="password" className='inputs'/>
+                <input ref={password} type="password" className='inputs' name='password' required/>
                 <img src={passwordIcon} alt="passwordIcon" className='icon'/>
         </fieldset>
 
         <fieldset className='innerFormulario'>
             <legend>Confirm Password</legend>
-                <input type="password" className='inputs'/>
+                <input ref={password} type="password" className='inputs' name='password' required/>
                 <img src={passwordIcon} alt="passwordIcon" className='icon'/>
         </fieldset>
 
