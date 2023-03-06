16a83aeae970feb8f619ae7207b466fbf41e1ed7
Alexis Franco <ronaldofranco196@gmail.com>
2023-03-06 18:49:28 -0300

    formChapter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css';
+import FormChapter from './components/FormChapter/FormChapter';
 import Index from './components/Index/Index';
 import IndexLayout from './layouts/IndexLayout/IndexLayout';
 
@@ -7,6 +8,7 @@ function App() {
     <div className="App">
       <IndexLayout>
         <Index/>
+        <FormChapter/>
       </IndexLayout>
     </div>
   );
