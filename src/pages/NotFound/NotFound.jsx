3db6d40a8ef386fb0b03d295b63150190a831b04
Rodrigo <contactorodrigocabrera@gmail.com>
2023-03-06 20:36:34 -0300

    corrección en page Not Found

--- src/pages/NotFound/NotFound.jsx
+++ src/pages/NotFound/NotFound.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './notfound.css'
 import {Link as Anchor} from 'react-router-dom'
+import Hero from '../Hero/Hero'
 
 export default function NotFound() {
     return (
         <div className='NotFound'>
-            <h3>¡Oops! Página no encontrada.</h3>
-            <h4>Intenta nuevamente:</h4>
-            <Anchor to='/index'>Volver a Home</Anchor>
+            <div className='contenedor'> 
+                <h3>¡Oops! Página no encontrada.</h3>
+                <h4>Intenta nuevamente:</h4>
+                <Anchor to='/'>Volver a Home</Anchor>
+            </div>
         </div>
     )
 }
