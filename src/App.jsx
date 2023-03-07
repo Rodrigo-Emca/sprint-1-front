1b6cbeb323ada4d8b488c0d9a74a83d810555cb1
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-03-07 10:18:05 -0300

    M 08 dev (#10)
    * M 05 dev (#8)
    
    * Inicio de M-05 para trabajo seguro
    
    * rutas creadas con react-router-dom y conectadas con Link de router-dom
    
    * correccion en formulario sign in, mensaje de alerta de credenciales incorrectas
    
    * correcciones en la redireción del anchor go back to home page
    
    * Corrección en LayoutMain. Corrección de rutas. Corrección de responsive
    
    * Sign In envia TOKEN a LOCAL STORAGE
    
    * formChapter
    
    * M 05 dev (#9)
    
    * Inicio de M-05 para trabajo seguro
    
    * rutas creadas con react-router-dom y conectadas con Link de router-dom
    
    * correccion en formulario sign in, mensaje de alerta de credenciales incorrectas
    
    * correcciones en la redireción del anchor go back to home page
    
    * Corrección en LayoutMain. Corrección de rutas. Corrección de responsive
    
    * Sign In envia TOKEN a LOCAL STORAGE
    
    * menu cambia segun SIGN IN / LOG OUT
    
    * corrección en page Not Found
    
    * Se eliminan imports no usados
    
    * correcion rutas
    
    ---------
    
    Co-authored-by: Rodrigo Cabrera <118491820+Rodrigo-Emca@users.noreply.github.com>

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
 import './App.css';
-import Index from './components/Index/Index';
-import IndexLayout from './layouts/IndexLayout/IndexLayout';
+
+import {router} from './pages/index'
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <IndexLayout>
-        <Index/>
-      </IndexLayout>
+      <RouterProvider router={router} />
     </div>
   );
 }
