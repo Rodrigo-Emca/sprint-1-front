d4ea9229f732ef81c930d654604e612038f5169b
Rodrigo <contactorodrigocabrera@gmail.com>
2023-03-12 22:40:44 -0300

    Botones MANGA y CHAPTERS randerizan su contenido cada uno

--- src/pages/Manga/Manga.jsx
+++ src/pages/Manga/Manga.jsx
@@ -1,101 +1,169 @@
-// import React from 'react'
-// import './manga.css'
-// import { useEffect } from 'react'
-// import { useParams } from 'react-router-dom'
-// import { useSelector, useDispatch } from 'react-redux'
-// import mangaActions from '../../store/Manga/actions'
+import React, { useState, useEffect } from "react";
+import "./manga.css";
+import { useParams } from "react-router-dom";
+import { Link as Anchor } from 'react-router-dom'
+import axios from "axios";
 
-// import CHAPTERStats from '../../images/CHAPTERStats.png'
-// import CHAPTERreactions from '../../images/CHAPTERreactions.png'
-// import NarutoImage from '../../images/Naruto.png'
-
-// const { get_manga } = mangaActions
-
-// export default function Manga() {
-//     const { id } = useParams()
-//     console.log(id)
-
-//     console.log(useSelector(manga=>manga))
-
-//     const dispatch = useDispatch()
-//     let data= useSelector(store => store.manga.manga)
-//     console.log(data)
-
-//     // useEffect(
-//     //     ()=> 
-//     //         dispatch(get_manga({_id}))
-//     // )
-// }
-
-import React, { useState, useEffect } from 'react';
-import './manga.css'
-import { useParams } from 'react-router-dom';
-import axios from 'axios';
-
-import CHAPTERStats from '../../images/CHAPTERStats.png'
-import CHAPTERreactions from '../../images/CHAPTERreactions.png'
+import CHAPTERStats from "../../images/CHAPTERStats.png";
+import CHAPTERreactions from "../../images/CHAPTERreactions.png";
 
 export default function Manga() {
     const { id } = useParams();
-    const URLpeticion = 'http://localhost:8000/mangas/:_id?_id='
-    console.log(id)
+    const URLpeticionManga = "http://localhost:8000/mangas/:_id?_id=";
     const [MANGA, setManga] = useState(null);
 
     useEffect(() => {
-        axios.get(URLpeticion + id)
-            .then((response) => {
-                setManga(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+        axios
+        .get(URLpeticionManga + id)
+        .then((response) => {
+            setManga(response.data.mangas);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     }, []);
 
-    let titleManga = MANGA ? MANGA.mangas.title : "";
-    let descriptionManga = MANGA ? MANGA.mangas.decription : "";
-    let imageManga = MANGA ? MANGA.mangas.cover_photo : "";
+    let titleManga = MANGA ? MANGA.title : "";
+    let descriptionManga = MANGA ? MANGA.decription : "";
+    let imageManga = MANGA ? MANGA.cover_photo : "";
+
+    //Para mostrar los detalles cada vez que se aprete el boton MANGA.
+    // const handleMostrarDetallesClick = () => {
+    //     axios
+    //     .get(URLpeticionManga + id)
+    //     .then((response) => {
+    //         setManga(response.data.mangas);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // };
+
+
+
+    //Para mostrar los capitulos cada vez que se aprete el boton CHAPTERS.
+
+    const URLpeticionChapters = "http://localhost:8000/chapters/chapters?manga_id=";
+    const [CHAPTERS, setChapters] = useState(null);
+    const [mostrarChapters, setMostrarChapters] = useState(false);
+
+    // useEffect(() => {
+    //     axios
+    //     .get(URLpeticionChapters + id)
+    //     .then((response) => {
+    //         setChapters(response.data.chapters);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // }, []);
+
+    // const handleMostrarChaptersClick = () => {
+    //     axios
+    //     .get(URLpeticionChapters + id)
+    //     .then((response) => {
+    //         setChapters(response.data.chapters);
+    //         setMostrarChapters(false);
+    //     })
+    //     .catch((error) => {
+    //         console.log(error);
+    //     });
+    // };
+
+    const handleMostrarChaptersClick = () => {
+        axios
+        .get(URLpeticionChapters + id)
+        .then((response) => {
+            setChapters(response.data.chapters);
+            setMostrarChapters(true);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+    };
     
-    const handleMostrarClick = () => {
-        axios.get(URLpeticion + id)
-            .then((response) => {
-                console.log(response.data.mangas)
-                setManga(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
+    const handleMostrarDetallesClick = () => {
+        axios
+        .get(URLpeticionManga + id)
+        .then((response) => {
+            setManga(response.data.mangas);
+            setMostrarChapters(false);
+        })
+        .catch((error) => {
+            console.log(error);
+        });
     };
 
     return (
-        <div className='contenedorGeneral'>
-            <div className='primerContenedor'>
+        <div className="contenedorGeneral">
+            <div className="primerContenedor">
                 <div>
-                    <img src={imageManga} alt="MangaImage" className='mangaImagen'/>
+                <img src={imageManga} alt="MangaImage" className="mangaImagen" />
                 </div>
                 <div>
-                    <h1>{titleManga}</h1>
+                <h1>{titleManga}</h1>
                 </div>
-                <div className='statsReactions'>
-                    <img src={CHAPTERStats} alt="CHAPTERStats" />
-                    <img src={CHAPTERreactions} alt="CHAPTERreactions" />
+                <div className="statsReactions">
+                <img src={CHAPTERStats} alt="CHAPTERStats" />
+                <img src={CHAPTERreactions} alt="CHAPTERreactions" />
                 </div>
             </div>
-            <div className='conenedorDeBotones'> 
-                <button>CHAPTERS</button>
+            <div className="conenedorDeBotones">
+                <button onClick={handleMostrarDetallesClick} className="botones">
+                MANGA
+                </button>
+                <button onClick={handleMostrarChaptersClick} className="botones">
+                CHAPTERS
+                </button>
             </div>
-            <div className='segundoContenedor'> 
-                <button onClick={handleMostrarClick}>MANGA</button>
-                {/* Contiene dos opciones para randerizar: */}
+            {!mostrarChapters && (
+                <div className="contenedorDescription">
+                    <div>
+                        <p>{descriptionManga}</p>
+                    </div>
+                </div>
+            )}
+
+            {mostrarChapters && (
+                <div className="contenedorChapters">
+                    <div>
+                        {CHAPTERS && CHAPTERS.map((chapter, index) => (
+                            <div key={index} className="innerContenedorChapter">
+                                <img src={imageManga} alt={chapter.title}  className="chapterImage"/>
+                                <div>
+                                    <p>Title: {chapter.title}</p>
+                                    <p>Order: {chapter.order}</p>
+                                </div>
+                                <Anchor to={"/chapter/"+id} className='btn-read'>Read</Anchor>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+    )}
+    </div>
+    )
+}
+
+{/* 
+            <div className="contenedorDescription">
                 <div>
-                    
                     <p>{descriptionManga}</p>
                 </div>
+            </div>
+            <div className="contenedorChapters">
                 <div>
-                    <div>
-                        {/* Un div por cada capitulo + boton para leer */}
-                    </div>
+                    {CHAPTERS && CHAPTERS.map((chapter, index) => (
+                        <div key={index} className="innerContenedorChapter">
+                            <img src={imageManga} alt={chapter.title}  className="chapterImage"/>
+                            <div>
+                                <p>Title: {chapter.title}</p>
+                                <p>Order: {chapter.order}</p>
+                            </div>
+                            <Anchor to={"/chapter/"+id} className='btn-read'>Read</Anchor>
+                        </div>
+                    ))}
                 </div>
             </div>
         </div>
-    )
-}
+    );
+} */}
